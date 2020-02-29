9c9f3fba29aa18d2fedbae1c9725409ffcc0fcce
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2020-03-01 01:19:38 +0600

    Add files via upload

--- PHP_OOP/index.php
+++ PHP_OOP/index.php
@@ -4,6 +4,7 @@ class User{
 
 public $age = 0;
 public $name;
+public $password;
 public function __construct($name,$age){
 $this->name = $name;
 echo "name :".$name. "<br>";
@@ -15,10 +16,25 @@ public function getAge()
 echo "Age : " . $this->age;
 }
 
+public function getPass($hint)
+{
+if($hint == 'getit')
+{
+return $this->password;
+}
+else{
+return 'wrong pass';
+}
+
+}
+
+
 }
 
 $PKS = new User("pks",21);
 $PKS->getAge();
+
+echo $PKS->getPass('getit');
 
 ?>
 
