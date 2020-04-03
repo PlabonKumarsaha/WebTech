8192e15a1e8bb423652ea5c4651943ba854ddf88
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2020-04-03 19:39:35 +0600

    Add files via upload

---
+++ project/doctorProfile.php
@@ -0,0 +1,223 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel ="stylesheet" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" type="text/css" href="css/Style.css/">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <header>
+    <div class ="container">
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="img/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="logo">
+    Autism Care
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent" style = " padding-left: 420px;">
+    <ul class="navbar-nav mr-auto">
+      
+      <li class="nav-item dropdown" class ="provider">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          For Providers
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Autism care Prime</a>
+          <a class="dropdown-item" href="#">Software for providers</a>
+          <a class="dropdown-item" href="#">corporate wellness</a>
+        </div>
+      </li>
+       <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Security & help
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Data security</a>
+          <a class="dropdown-item" href="#">Help</a>   
+        </div>
+      </li>
+      <li class="nav-item" style = " padding-left: 120px; padding-top: 7px;">
+        <form class="form-inline">
+    <button class="btn btn-sm btn-outline-secondary" type="button">Login/Signup</button>
+  </form>
+      </li>
+      
+    </ul>
+   
+  </div>
+</nav>
+    </div>
+
+
+    <nav class="navbar navbar-light bg-light">
+  
+  <form class="form-inline" style = " padding-left: 80px;">
+     
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">@</span>
+      </div>
+      <input type="text" class="form-control" placeholder="Search Location" aria-label="Username" aria-describedby="basic-addon1">
+    </div>
+  <div style = " padding-left: 40px;  @gridColumnWidth = 65px @gridGutterWidth = 20px"> 
+     <input class="form-control mr-sm-6" type="search" placeholder="Search,doctors,hospitals,clinics,etc." aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+
+     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      
+      
+       <li><a class="nav-link" href="#">Feed up for endless wait? <br> Look for clinic with <style= "color:green">Prime</style=><span class="sr-only">(current)</span></a>
+      </li>
+    
+    </ul>
+  </div>
+  </form>
+</nav>
+
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item"><a href="#">Banglaore</a></li>
+    <li class="breadcrumb-item"><a href="#">Gynocologist</a></li>
+    <li class="breadcrumb-item"><a href="#">BTM layout2</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Dr suman sing</li>
+  </ol>
+</nav>
+
+    </header>
+
+
+
+<div id="wrapper">
+
+
+<div id ="left_div">
+
+    <div class="card mb-3" style="max-width: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="img/doc.jpg" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title" style="text-align:left;float:left;">Dr. Suman Singh<font size="3px"> <a href="#"><small class="text-muted" style="padding-top: 8px;text-align:right;float:right;">Profile is claimed</small></a></font></h5>
+
+        <p class="card-text"style="text-align:left;"><br>  MBBS, MD - Obstetrics & Gynaecology
+Gynecologist, Obstetrician, Infertility Specialist, 25 Years Experience Overall  (21 years as specialist)</p>
+        <p class="card-text"><small class="text-muted">Medical vaified</small></p>
+
+        <span style="display: inline-block;"><p>Dr. Suman Singh is a class topper throughout, MBBS from Mysore Medical College with first-class,</p>
+          <details style="text-align:left;float:right; display: inline-block;">
+            
+  <summary>view More details</summary>
+  <p> - with first-class, internship from St. John’s Hospital Bangalore in 1995, MD in OBG from Bangalore Medical College in 1999 and later worked at Breach Candy Hospital, Mumbai awarded the fellowship degree (FGO) by society for advanced studies in Medical Sciences, New Delhi in 2000. Worked with Dr. Gunasheela, Bangalore in the IVF/fertility unit in 2004. Worked as a consultant in Apollo, Fortis, Sagar, Manipal Fertility, Cloudnine Hospitals. Her areas of special interest are infertility and high-risk pregnancy and believe in doing her job with compassion and high standards of patient care.</p>
+
+</details>
+        </span>
+        <a href="#" class="btn btn-primary btn-sm  float-right" role="button" aria-pressed="true">submit feedback</a>
+      </div>
+    </div>
+  </div>
+
+
+
+  <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</a>
+  </li>
+</ul>
+<div class="tab-content" id="pills-tabContent">
+  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
+  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
+  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+</div>
+</div>
+
+<!--left div ends -->
+
+<div>
+
+  <div id= "right_div">
+
+
+
+    <div class="card">
+  <div class="card-header">
+    pick time slot
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Manasi Clinic</h5>
+    <font size="2px"><p>4.5* ₹500 <b style="color:green;"> Max 30 mins wait</b></p></font>
+    <font size="2px"><p>BTM Layout 2nd Stage</p></font>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <p class="card-text"><font size="3px"><b>Encourage social distancing and for <br>non-emergency issues consult doctors online</b></font></p>
+    <button type="button" class="btn btn-lg btn-primary" >Consult Online now</button>
+
+    <div class="card" style="width: 18rem;">
+  <div class="card-body" style= " background-color: lightblue;">
+    <p class="card-text"><font size="1.9px">Due to Nationwide lockdown some centres are not providing appointment booking. We encourage you to do online consultation for non emergency cases. For emergency cases please call & verify No: +918047111547.</font></p>
+    
+  </div>
+
+  <div class="grid-container">
+  <div class="row ">
+    <div class="col-4">4.50pm</div>
+    <div class="col-4">6.10pm</div>
+    <div class="col-4">6.40pm</div>
+    <div class="col-4">7.50pm</div>
+    <div class="col-4">8.30pm</div>
+    <div class="col-4">9am</div>
+    <div class="col-4">10am</div>
+    <div class="col-4">11am</div>
+
+    <button type="button" class="btn btn-lg btn-primary" >book now</button>
+
+  </div>
+</div>
+</div>
+    
+  </div>
+</div>
+
+
+<!--right div ends -->
+  </div>
+
+
+<!--wrapper div ends -->
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+
+
+
+
+  </body>
+</html>
