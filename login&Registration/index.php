a7644d69d07c5e496628ed57a2b5b75e5206b61e
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2020-04-07 02:12:05 +0600

    Add files via upload

---
+++ login&Registration/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login & Registration</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div id="wrapper">
+	<div id="header">
+		
+		<h1>Welcome to the page!</h1>
+	</div>
+	<div id="content">
+		<h1>Our services</h1>
+		<p>
+			ja iccha likha ace ja iccha likha ace..altu phalytujnjadnkajd dbajsnam djcbskjdbkasnd abdajdajdnjadnajdnadn nadajdnajdj ajd ajdnak aansajsnjs qjsqjsqj sq qq  q d  dd ajdajdnjan
+		</p>
+
+	</div>
+	<div id="footer">
+copywrite 2020!
+	</div>
+
+
+	<div>
+	
+</body>
+</html>
---
+++ login&Registration/login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login & Registration</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div id="wrapper">
+	<div id="header">
+		
+		<h1>Welcome to the page!</h1>
+	</div>
+	<div id="content">
+	
+
+	<div id="loginBoxes">
+
+        <center>
+
+		<form action="login_core.php">
+			<h2>Login Form</h2>
+
+			<input type="text" placeholder="Username" name="username"><br><br>
+			<input type="password" placeholder="password" name="pwd"><br><br>
+			<input type="submit" id="loginbutton" value="Login" name="loginBtn"><br><br>
+			
+			
+         </center>
+		</form>
+
+
+	</div>
+
+	</div>
+	<div id="footer">
+copywrite 2020!
+	</div>
+
+
+	<div>
+	
+</body>
+</html>
